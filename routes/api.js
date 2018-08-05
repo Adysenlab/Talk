061ba69f6c125868605250ebf0c37c43df0c27e5
Aditya Aloy Sen <aloysen951@gmail.com>
2018-08-05 23:45:54 +0530

    trying to integrate ... message queue mechanisms

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var amqp = require('amqplib/callback_api')
 var router = express.Router();
 
 /* GET home page. */
@@ -8,4 +9,21 @@ router.get('/', function(req, res, next) {
   })
 });
 
+// make a betting application
+router.get('/bet', callD_alembert);
+
+function callD_alembert(req, res) {
+  // using spawn instead of exec, prefer a stream over a buffer
+  // to avoid maxBuffer issue
+  var spawn = require("child_process").spawn;
+  var process = spawn('python', ["../api/betting/init.py",
+    req.query.funds, // starting funds
+    req.query.size, // (initial) wager size
+    req.query.count, // wager count — number of wagers per sim
+    req.query.sims // number of simulations
+  ]);
+  process.stdout.on('data', function (data) {
+    res.send(data.toString());
+  });
+}
 module.exports = router;
